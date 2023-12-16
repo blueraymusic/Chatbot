698e758444d43c8d62e33ecf4407e5d555d180a8
Blueray <83096078+blueraymusic@users.noreply.github.com>
2023-12-16 19:30:30 +0300

    Update query_search.js

--- Chatbot files/query_search.js
+++ Chatbot files/query_search.js
@@ -7,21 +7,21 @@ const form = document.querySelector(".header__form");
       const inputValue = input.value.toLowerCase();
   
       if (inputValue.includes("start")) {
-        window.location.href = "file:///C://Users//minas//Desktop//vscode%20(Adel//Chatbot%20files//Get_started.html";
+        window.location.href = "Get_started.html";
       } else if (inputValue.includes("document")) {
-        window.location.href = "file:///C://Users//minas//Desktop//vscode%20(Adel//Chatbot%20files//Documentation.html";
+        window.location.href = "Documentation.html";
       } else if (inputValue.includes("mission") || inputValue.includes("vision") || inputValue.includes("company") || inputValue.includes("about you")){
-        window.location.href = "file:///C://Users//minas//Desktop//vscode%20(Adel//Chatbot%20files//AboutUs.html";
+        window.location.href = "AboutUs.html";
       } else if (inputValue.includes("home") || inputValue.includes("main")){
-         window.location.href = "file:///C://Users//minas//Desktop//vscode%20(Adel//Chatbot%20files//Intro%20page.html"; 
+         window.location.href = "Intro%20page.html"; 
       } else if (inputValue.includes("code") || inputValue.includes("source")|| inputValue.includes("web")){
-         window.location.href = "file:///C://Users//minas//Desktop//vscode%20(Adel//Chatbot%20files//code.html";
+         window.location.href = "code.html";
       } else if (inputValue.includes("contact") || inputValue.includes("report")|| inputValue.includes("problem")|| inputValue.includes("issue")|| inputValue.includes("error")|| inputValue.includes("work")){
-        window.location.href = "file:///C://Users//minas//Desktop//vscode%20(Adel//Chatbot%20files//contact_form.html";
+        window.location.href = "contact_form.html";
       } else if (inputValue.includes("feature") || inputValue.includes("function")|| inputValue.includes("secret")|| inputValue.includes("research")|| inputValue.includes("google")|| inputValue.includes("keys")){
-          window.location.href = "file:///C://Users//minas//Desktop//vscode%20(Adel//Chatbot%20files//cheat_keys.html";
+          window.location.href = "cheat_keys.html";
       } else{
-        window.location.href = "file:///C://Users//minas//Desktop//vscode%20(Adel//Chatbot%20files//NoPageFound.html";
+        window.location.href = "NoPageFound.html";
       }
 
     });
